1cdab31282fbace2fab4bcc105774b523ac0a787
Amittha1508 <korttumamittha@gmail.com>
2025-11-23 17:31:41 +0700

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'memoriesApp',
+  webDir: 'dist'
+};
+
+export default config;
